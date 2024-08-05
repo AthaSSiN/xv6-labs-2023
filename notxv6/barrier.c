904f4ebaff1c509338d9e774252b7569b3420e0d
AthaSSiN <athassin.asp@gmail.com>
2024-08-05 16:46:58 +0530

    complete barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -20,6 +20,7 @@ barrier_init(void)
   assert(pthread_mutex_init(&bstate.barrier_mutex, NULL) == 0);
   assert(pthread_cond_init(&bstate.barrier_cond, NULL) == 0);
   bstate.nthread = 0;
+  bstate.round = 0; // is this needed?
 }
 
 static void 
@@ -30,6 +31,40 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
+
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  
+  // helps hold a thread which has raced to this point, as we wait till nthread is zero again 
+  // (first check is for round 0 and if someone passes through, for others to wakeup and pass)
+  while(bstate.round > round && bstate.nthread > 0) 
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+
+  // locks here so that only one thread at a time can check and update
+  // putting locks inside might cause multple incorect increaments in all cases
+  // I don't think it will be wrong in this case though, will see
+  
+  // WRONG: No need for separate locks, cond_wait anyway releases locks held earlier
+  if(bstate.nthread == 0 && bstate.round > round){
+    round++;
+    pthread_cond_broadcast(&bstate.barrier_cond); // as there are two sleeps, we will need two wakeups
+  }
+  // update the nthread
+  bstate.nthread++;
+  
+  // second statement is to wait till nthreads are the same
+  // first statement to let the woken up processes pass through
+  while(bstate.round <= round && bstate.nthread < nthread)
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+
+
+  if(bstate.nthread == nthread) {
+    // can start next round now
+    bstate.round++;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+    // bstate.nthread = 0;
+  }
+  bstate.nthread--;
+  pthread_mutex_unlock(&bstate.barrier_mutex);
   
 }
 
@@ -44,6 +79,7 @@ thread(void *xa)
     int t = bstate.round;
     assert (i == t);
     barrier();
+    // printf("%ld: %d, %d\n", n, i, bstate.nthread);
     usleep(random() % 100);
   }
 
