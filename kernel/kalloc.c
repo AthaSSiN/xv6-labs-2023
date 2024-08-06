ddb4c6329e58e7ec978608d63fddec41f0b20e85
AthaSSiN <athassin.asp@gmail.com>
2024-08-07 00:12:28 +0530

    complete memory allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -10,6 +10,7 @@
 #include "defs.h"
 
 void freerange(void *pa_start, void *pa_end);
+void kfree2(void* pa, int cpu);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
@@ -18,15 +19,19 @@ struct run {
   struct run *next;
 };
 
-struct {
+struct kmem {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+};
+
+struct kmem kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i = 0; i < NCPU; ++i) {
+    initlock(&kmem[i].lock, "kmem");
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,8 +40,13 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+
+  uint64 total_mem_per_cpu = ((uint64)pa_end - (uint64)pa_start) / NCPU;
+
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
+    // not actually needed this way but i prefer equitable distrib here
+    kfree2(p, (int)(((uint64)p - (uint64)pa_start) / total_mem_per_cpu));
+  }
 }
 
 // Free the page of physical memory pointed at by pa,
@@ -45,6 +55,18 @@ freerange(void *pa_start, void *pa_end)
 // initializing the allocator; see kinit above.)
 void
 kfree(void *pa)
+{
+  // calling and using cpuid needs interrups off
+  // internet solution hasn't used this and it breaks test3
+  // it passes but there is a usertrap
+  push_off();
+  int cpu = cpuid();
+  kfree2(pa, cpu);
+  pop_off();
+}
+
+void
+kfree2(void* pa, int cpu)
 {
   struct run *r;
 
@@ -56,10 +78,10 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&kmem[cpu].lock);
+  r->next = kmem[cpu].freelist;
+  kmem[cpu].freelist = r;
+  release(&kmem[cpu].lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,13 +90,38 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
+  push_off();
+  int cpu = cpuid();
+  
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  acquire(&kmem[cpu].lock);
+  r = kmem[cpu].freelist;
+
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[cpu].freelist = r->next;
+  release(&kmem[cpu].lock);
+
+  if(!r) {
+    for(int i = cpu + 1;; ++i) { 
+      if(i == NCPU)
+        i = 0;
+      // printf("%d %d\n", i, cpu);
+      if(i == cpu)
+        break;
+      // steal free pages
+      acquire(&kmem[i].lock);
+      r = kmem[i].freelist;
+      if(r) {
+        kmem[i].freelist = r->next;
+        r->next = 0; // not necessary but makes it clearer
+        release(&kmem[i].lock);     
+        break;
+      }
+      release(&kmem[i].lock);     
+    }
+  }
+  pop_off();
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
